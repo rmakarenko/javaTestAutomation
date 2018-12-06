5cd16b17d9b4a003c375339944f7592774f68237
roman.makarenko <roman.makarenko.spb@gmail.com>
2018-12-06 23:39:56 +0300

    Hi guys, this is our first step to success. Hope, we wont stop.

---
+++ src/test/java/avitoTest/SearchFirst.java
@@ -0,0 +1,44 @@
+package avitoTest;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SearchFirst {
+
+    @Test
+
+    public void SearchCategory () {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/roman/chromedriver_win32/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.avito.ru/sankt-peterburg");
+
+        //WebElement elementCategory = driver.findElement(By.name("category_id"));
+        //elementCategory.click();
+
+        WebElement elementCategoryValue = driver.findElement(By.partialLinkText("Билеты и путешествия"));
+        elementCategoryValue.click();
+
+        WebElement elementCategoryMetro = driver.findElement(By.xpath(".//*[@id='directions']/div"));
+        elementCategoryMetro.click();
+
+        WebElement elementCategoryDeviatkino = driver.findElement(By.xpath("svg:svg/svg:g/svg:text[36]"));
+        elementCategoryDeviatkino.click();
+
+
+
+        //elementPassword.sendKeys("console123");
+
+        //driver.close();
+
+
+    }
+}
---
+++ src/test/java/avitoTest/avitoOpener.java
@@ -0,0 +1,44 @@
+package avitoTest;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+public class avitoOpener {
+
+    @Test
+
+    public void navigateToAvito() {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/roman/chromedriver_win32/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.avito.ru/sankt-peterburg");
+
+
+        WebElement element = driver.findElement(By.cssSelector(".header-services-menu-link-IZnN3.header-services-menu-link-not-authenticated-1TAH-"));
+        element.click();
+
+        WebElement elementLogin = driver.findElement(By.name("login"));
+        elementLogin.click();
+        elementLogin.sendKeys("89516581827");
+
+        WebElement elementPassword = driver.findElement(By.name("password"));
+        elementPassword.click();
+        elementPassword.sendKeys("console123");
+
+        WebElement elementEnter = driver.findElement(By.name("submit"));
+        elementEnter.click();
+
+        //driver.close();
+    }}
+
